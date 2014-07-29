809256d0b83f89d4ada33117db603d2ecaf750f6
Piotr Klosinski <mybettingstuff@gmail.com>
2014-07-29 19:03:42 +0200

    minor code changes made while going over the solution video

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:email]
+    #user = User.where(email: params[:email]).first <-- this is the solution code
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
@@ -16,6 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    flash[:success] = "You have signed out"
     redirect_to root_path
   end
 end
