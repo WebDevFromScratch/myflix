908cd559f20c33b7aaad7f1a3419fedc4d459707
Piotr Klosinski <mybettingstuff@gmail.com>
2014-08-20 16:32:36 +0200

    added ratings to video show view

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe ReviewsController do
   describe "POST create" do
     context "with valid input" do
-      it "creates the review"
+      it "creates the review" do
+        
+      end
+      
       it "sets a user for the review"
       it "sets a video for the review"
       it "redirects to review's video page"
