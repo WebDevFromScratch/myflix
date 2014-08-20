f9965295b8c99c4f961ef7c2f16cbf467366a420
Piotr Klosinski <mybettingstuff@gmail.com>
2014-08-20 11:13:17 +0200

    added tests for users controller (new and create actions)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,28 @@ describe UsersController do
     it 'sets the @user variable' do
       #setup
       #action
+      get :new
       #verification
+      #(assigns(:user)).to be_a_new(User)
+      expect(assigns(:user)).to be_new_record
+      expect(assigns(:user)).to be_instance_of(User)
+    end
+  end
+
+  describe 'POST create' do
+    it "creates the user record in the database with valid attributes" do
+      #user = Fabricate(:user)
+      post :create, user: Fabricate.attributes_for(:user)
+      expect(User.count).to eq(1)
+    end
+    #flash message?
+    it "redirects to sign in page with valid attributes" do
+      post :create, user: Fabricate.attributes_for(:user)
+      expect(response).to redirect_to(sign_in_path)
+    end
+    it "renders the new template with invalid attributes" do
+      post :create, user: Fabricate.attributes_for(:user, email: "")
+      expect(response).to render_template(:new)
     end
   end
 end
